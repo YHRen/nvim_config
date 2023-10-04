0e73e15e6e5935c7d9b0c47a1db2065395b82697
Yihui (Ray) Ren <yren@bnl.gov>
2023-10-04 10:47:21 -0400

    replace vimwiki with plug.vim

---
+++ after/plugin/kiwi.rc.lua
@@ -0,0 +1,22 @@
+local ok, _ = pcall(require, 'kiwi')
+
+-- Setup Custom wiki path if required
+if not ok then
+    print("kiwi not loaded")
+    return
+end
+require('kiwi').setup({
+  {
+    name = "work",
+    path = vim.env.HOME .. "/.config/syncdata/vimwiki/kiwi_notes"
+  }
+})
+
+-- Use default path (i.e. ~/wiki/)
+local kiwi = require('kiwi')
+
+-- Necessary keybindings
+vim.keymap.set('n', '<leader>ww', kiwi.open_wiki_index, {})
+vim.keymap.set('n', '<leader>wd', kiwi.open_diary_index, {})
+vim.keymap.set('n', '<leader>wn', kiwi.open_diary_new, {})
+vim.keymap.set('n', '<leader-x>', kiwi.todo.toggle, {})
