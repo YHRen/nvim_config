bb200b3e0658f08a26425d7fd0118255c07efc57
Ray <yihui.ren@gmail.com>
2023-11-21 23:10:14 -0500

    update kiwi rc

--- after/plugin/kiwi.rc.lua
+++ after/plugin/kiwi.rc.lua
@@ -17,6 +17,5 @@ local kiwi = require('kiwi')
 
 -- Necessary keybindings
 vim.keymap.set('n', '<leader>ww', kiwi.open_wiki_index, {})
-vim.keymap.set('n', '<leader>wd', kiwi.open_diary_index, {})
-vim.keymap.set('n', '<leader>wn', kiwi.open_diary_new, {})
 vim.keymap.set('n', '<leader-x>', kiwi.todo.toggle, {})
+
